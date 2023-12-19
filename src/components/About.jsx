d346946d6714be5193add5421e9faf7a002e231f
kasun-chamara <kasunchamarawb@gmail.com>
2023-12-20 02:04:24 +0530

    change aboutpage

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
               <img
                 src="https://www.pngplay.com/wp-content/uploads/7/Global-Networking-Transparent-PNG.png"
                 alt=""
-                className="object-cover w-full pb-14 "
+                className="object-cover w-full "
               />
             </div>
           </div>
